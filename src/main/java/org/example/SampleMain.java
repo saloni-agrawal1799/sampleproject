a76727c01fcfa3073d3e78c152420c3c0ddaca86
saloni-agrawal1799 <saloni.agrawal_ftc@external.swiggy.in>
2024-09-06 12:20:34 +0530

    Push from different branch to main branch

---
+++ src/main/java/org/example/SampleMain.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class SampleMain {
+
+    public static void main(String[] args) {
+        int a = 4,b = 5;
+        int c = a+b;
+        System.out.println(c);
+    }
+
+}
